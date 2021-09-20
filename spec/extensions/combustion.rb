d3037834818987f51849460c0f339c699e5e2df2
Conor Mongey <conor@mongey.net>
2021-09-20 17:13:12 +0100

    Remove combustion monkey patch

--- spec/extensions/combustion.rb
+++
@@ -1,9 +0,0 @@
-class Combustion::Database::Reset
-  def call
-    configuration = resettable_db_configs.to_h[Rails.env]
-    adapter = configuration["adapter"] ||
-              configuration["url"].split("://").first
-
-    operator_class(adapter).new(configuration).reset
-  end
-end
