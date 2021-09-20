8aa21aa42a587c8c2dc958015574887dbae2aba7
Conor Mongey <conor@mongey.net>
2021-09-20 19:37:14 +0100

    cleanup a bit

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,20 +4,24 @@ require "yaml"
 
 Dir["#{File.dirname(__FILE__)}/extensions/*.rb"].sort.each { |f| require f }
 Dir["#{File.dirname(__FILE__)}/support/*.rb"].sort.each { |f| require f }
+DATABASE_CONFIG_LOCATION = "spec/internal/config/database.yml"
 
-original_env = Rails.env
-adapter_name = if Gemika::Env.gem?("pg")
-  "postgresql"
-elsif Gemika::Env.gem?("mysql2")
-  "mysql"
+def adapter_name
+  if Gemika::Env.gem?("pg")
+    "postgresql"
+  elsif Gemika::Env.gem?("mysql2")
+    "mysql"
+  end
 end
 
-puts "{attempintg to use the #{adapter_name} adapter"
-file_location = "spec/internal/config/database.yml"
-puts File.read(file_location)
-config = YAML.load_file(file_location)
+def database_config_from_gems(file_location)
+  config = YAML.load_file(file_location)
+  config.slice(adapter_name)
+end
 
-File.write(file_location, config.slice(adapter_name).to_yaml)
+original_env = Rails.env
+File.write(DATABASE_CONFIG_LOCATION,
+  database_config_from_gems(DATABASE_CONFIG_LOCATION).to_yaml)
 
 Rails.env = adapter_name
 Combustion.initialize! :active_record
