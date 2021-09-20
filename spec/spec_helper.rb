72e837bbf0dfb72464485fa465fa680cd43efb3f
Conor Mongey <conor@mongey.net>
2021-09-20 18:13:54 +0100

    hmm

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,10 @@
 require 'gemika'
 require 'combustion'
 
-Dir["#{File.dirname(__FILE__)}/extensions/*.rb"].sort.each {|f| require f}
-Dir["#{File.dirname(__FILE__)}/support/*.rb"].sort.each {|f| require f}
+Dir["#{File.dirname(__FILE__)}/extensions/*.rb"].sort.each { |f| require f }
+Dir["#{File.dirname(__FILE__)}/support/*.rb"].sort.each { |f| require f }
 
-Combustion.initialize!
-#:active_record do
-# Rails.env = TemporalTables::DatabaseAdapter.adapter_name
-# end
-
+Combustion.initialize! :all
 DatabaseCleaner.strategy = :deletion
 
 RSpec.configure do |config|
