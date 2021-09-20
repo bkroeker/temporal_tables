f1c5b2c9e44f069c3d55e4f0ff398321d9992ce5
Conor Mongey <conor@mongey.net>
2021-09-20 18:34:41 +0100

    oh, that's why that was there

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,19 @@
-require 'gemika'
-require 'combustion'
+require "gemika"
+require "combustion"
 
 Dir["#{File.dirname(__FILE__)}/extensions/*.rb"].sort.each { |f| require f }
 Dir["#{File.dirname(__FILE__)}/support/*.rb"].sort.each { |f| require f }
 
-Combustion.initialize! :all
+original_env = Rails.env
+adapter_name = if Gemika::Env.gem?("pg")
+  "postgresql"
+elsif Gemika::Env.gem?("mysql2")
+  "mysql"
+end
+Rails.env = adapter_name
+Combustion.initialize! :active_record
 DatabaseCleaner.strategy = :deletion
+Rails.env = original_env
 
 RSpec.configure do |config|
   config.before(:each) do
