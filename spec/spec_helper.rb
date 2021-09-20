af7ed24936c59d53e3e95f78f2ee0b62657c678a
Conor Mongey <conor@mongey.net>
2021-09-20 18:42:49 +0100

    wut

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require "gemika"
 require "combustion"
+require "yaml"
 
 Dir["#{File.dirname(__FILE__)}/extensions/*.rb"].sort.each { |f| require f }
 Dir["#{File.dirname(__FILE__)}/support/*.rb"].sort.each { |f| require f }
@@ -10,6 +11,14 @@ adapter_name = if Gemika::Env.gem?("pg")
 elsif Gemika::Env.gem?("mysql2")
   "mysql"
 end
+
+puts "{attempintg to use the #{adapter_name} adapter"
+file_location = "spec/internal/config/database.yml"
+puts File.read(file_location)
+config = YAML.load_file(file_location)
+
+File.write(file_location, config.slice(adapter_name).to_yaml)
+
 Rails.env = adapter_name
 Combustion.initialize! :active_record
 DatabaseCleaner.strategy = :deletion
