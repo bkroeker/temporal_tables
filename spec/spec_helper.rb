9aae7c45da780027e8803c88dae044809ff23a69
Conor Mongey <conor@mongey.net>
2021-09-20 17:29:52 +0100

    hmm

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,9 +4,10 @@ require 'combustion'
 Dir["#{File.dirname(__FILE__)}/extensions/*.rb"].sort.each {|f| require f}
 Dir["#{File.dirname(__FILE__)}/support/*.rb"].sort.each {|f| require f}
 
-Combustion.initialize! :active_record do
-  Rails.env = TemporalTables::DatabaseAdapter.adapter_name
-end
+Combustion.initialize!
+#:active_record do
+# Rails.env = TemporalTables::DatabaseAdapter.adapter_name
+# end
 
 DatabaseCleaner.strategy = :deletion
 
